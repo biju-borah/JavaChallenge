d000728fe64fab9e85da8a71a6d4258ce58204d1
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-05-05 15:17:12 +0530

    Tree

---
+++ DSA/Tree/BinaryTree/Traversal/Postorder.java
@@ -0,0 +1,49 @@
+package Traversal;
+
+public class Postorder {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+    static class BinaryTree {
+        static int index = -1;
+
+        Node buildTree(int[] nodes) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+            Node node = new Node(nodes[index]);
+            node.left = buildTree(nodes);
+            node.right = buildTree(nodes);
+            return node;
+        }
+
+        void traversal(Node root) {
+            if (root == null) {
+                System.out.print(-1 + " ");
+                return;
+            }
+            traversal(root.left);
+            traversal(root.right);
+            System.out.print(root.data + " ");
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree binaryTree = new BinaryTree();
+        Node root = binaryTree.buildTree(nodes);
+        binaryTree.traversal(root);
+    }
+}
